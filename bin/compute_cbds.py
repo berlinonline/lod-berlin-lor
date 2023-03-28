f65ec8395f278eec297bd688336ca76dc8fd55d6
Knud Möller <dunken69@gmail.com>
2023-03-28 22:44:54 +0200

    fix selection of URIs in cbd script

--- bin/compute_cbds.py
+++ bin/compute_cbds.py
@@ -26,11 +26,14 @@ cbds = {}
 
 for subject in all.subjects():
     try:
-        namespace = split_uri(subject)[0]
+        namespace = str(subject)
+        if subject == args.base:
+            namespace = split_uri(subject)[0]
         if args.base in namespace:
             cbd = all.cbd(subject)
             name = subject.removeprefix(args.base)
             name = name.replace("/", "_")
+            name = f"_{name}"
             cbd_dict = json.loads(cbd.serialize(format="json-ld"))
             out_path = args.output / name
             outpath = f"{out_path}.json"
